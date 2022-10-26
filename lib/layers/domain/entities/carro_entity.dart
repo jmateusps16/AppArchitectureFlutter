7d216fa8424a4343082de0302b18145fc5702511
Mateus Palmeira <jmateusps16@gmail.com>
2022-10-26 20:20:33 -0300

    Exemplo de regra de negócio interna da entidade

--- lib/layers/domain/entities/carro_entity.dart
+++ lib/layers/domain/entities/carro_entity.dart
@@ -4,4 +4,11 @@ class Carro {
   double valor;
 
   Carro({required this.placa, required this.qtdPortas, required this.valor});
+
+  /// Exemplo de regra de negócio:
+  /// Calcular o valor do carro pela quantidade de portas que ele possui.
+  /// Neste caso é uma regra de negócio que não depende de outra entidade, por isso está dentro dela mesmo.
+  double get valorReal {
+    return valor * qtdPortas;
+  }
 }
